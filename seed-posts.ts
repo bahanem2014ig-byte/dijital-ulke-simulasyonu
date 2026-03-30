0e89d0f52fdd8471fef1ef4eefc0304cab9d9092
bahanem2014ig-byte <bahanem2014ig@gmail.com>
2026-03-30 16:49:43 +0300

    Updated seed-posts.ts with historical events and citizen comments

---
+++ seed-posts.ts
@@ -0,0 +1,41 @@
+const historicalPosts = [
+    {
+        era: "Renaissance",
+        event: "Invention of the Printing Press",
+        comments: [
+            "The printing press has transformed our ability to share knowledge!",
+            "I can finally own books and explore ideas beyond my village.",
+            "This will empower the common man!"
+        ]
+    },
+    {
+        era: "Industrial Revolution",
+        event: "Industrial Revolution",
+        comments: [
+            "The factories are changing our way of life, but at what cost?",
+            "It's exciting to see such rapid progress in technology.",
+            "We must address the challenges of working conditions!"
+        ]
+    },
+    {
+        era: "Late 20th Century",
+        event: "Rise of the Internet",
+        comments: [
+            "The internet is connecting people like never before!",
+            "Information is at our fingertips, but it's overwhelming.",
+            "This technology will change how we connect and learn."
+        ]
+    },
+    {
+        era: "Modern Day",
+        event: "Climate Crisis",
+        comments: [
+            "We need urgent action on climate change!",
+            "It's our duty to protect the planet for future generations.",
+            "I feel hopeless sometimes, but we must keep fighting!"
+        ]
+    }
+];
+
+// Exporting the updated historical posts
+export default historicalPosts;
